fb8fb5a5e260bfb2a5aed2f8c6f5bfda9dd1fe48
sanzot <santi.roman@gmail.com>
2013-07-30 09:03:26 +0000

    tercera iteracion sin Buffer

--- web.js
+++ web.js
@@ -2,14 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-
+var fs= require('fs');
 app.get('/', function(request, response) {
-    var fs = require('fs');
-    var buffer = new Buffer();
-    response.send(buffer.toString('utf-8', fs.readFileSync("index.html")));
+  response.send(fs.ReadFileSync('index2.html','utf-8');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
