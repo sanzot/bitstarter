efb7e48a9d7bf98a55c78c9c26db2b23c3f44065
Ubuntu <ubuntu@ip-172-31-44-192.us-west-2.compute.internal>
2013-08-15 17:19:21 +0000

    HW4 - adaptando web.js para que se ejecuten los update de index.htm sin tener que reiniciar servidores

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var htmlfile = "index.html";
 
-var fs= require('fs');
+var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
-
-var buf= new Buffer (fs.readFileSync('index2.html');
-
-  response.send(buf.toString('utf-8')
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
 
